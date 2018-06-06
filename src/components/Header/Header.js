4bef02286893abead8cf36ebdef680d0f4d3a96f
Sean McPheeters <alexissean@dhcp-v101-193.mobile.uci.edu>
2018-06-05 19:39:07 -0700

    Updated React App

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = props => (
   <div className="header">
     <div className="title">{props.children}</div>
     <div className="scores">
-      Score: {props.score} Highscore: {props.highscore}
+      Your Score: {props.score} Highscore: {props.highscore}
     </div>
   </div>
 );
