ef4c377ef66b23aa5d44b6cc6cc0a64b315e1c4d
Sean McPheeters <alexissean@dhcp-v101-193.mobile.uci.edu>
2018-06-05 19:44:53 -0700

    Updated Files

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = props => (
   <div className="header">
     <div className="title">{props.children}</div>
     <div className="scores">
-      Your Score: {props.score} Highest Score: {props.highscore}
+      Your Score:{props.score} Highest Score:{props.highscore}
     </div>
   </div>
 );
